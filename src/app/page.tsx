d086f695e3beb05443c1d3bf1ed273dcc55f6c76
rafaelmaia <rafaelmfcontato@protonmail.com>
2025-02-19 16:34:45 -0300

    aula 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
+import { Button } from '@/components/button'
+import { IconButton } from '@/components/icon-button'
+import { InputField, InputIcon, InputRoot } from '@/components/input'
+import { ArrowRight, Copy, Mail } from 'lucide-react'
+
 export default function Home() {
   return (
     <main>
       <div>Hello world!</div>
+      <Button type="submit">
+        Enviar
+        <ArrowRight />
+      </Button>
+
+      <IconButton>
+        <Copy />
+      </IconButton>
+
+      <div>
+        <InputRoot>
+          <InputIcon>
+            <Mail className="size-5" />
+          </InputIcon>
+          <InputField type="email" placeholder="Email" />
+        </InputRoot>
+      </div>
     </main>
-  );
+  )
 }
